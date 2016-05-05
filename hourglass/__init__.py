e2f142452744acba7dda248cfd07eb2a4bd720f1
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-05 18:18:55 -0500

    changing config to ini format
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/__init__.py
+++ hourglass/__init__.py
@@ -1,21 +1,24 @@
 from flask import Flask
-import json
+from flask.ext.iniconfig import INIConfig
+#from hourglass.cache import cache
 
 
-def create_app(config, mode):
+def create_app(config):
     app = Flask(__name__)
-    app.debug = True
-    app.config['hourglass_config'] = get_config()
+    #cache.init_app(app)
+    INIConfig(app)
+    app.config.from_inifile(config)
+    hourglass_config = app.config.get('hourglass')
+    enabled_regions = hourglass_config.get('enabled_regions').split(',')
+    app.config['regions'] = {}
+    for region in enabled_regions:
+        app.config['regions'][region] = app.config.get(region)
     register_blueprints(app)
     return app
 
 
-def get_config(path='./hourglass.json'):
-    with open('./hourglass.json') as conffile:
-        HGCONFIG = json.load(conffile)
-    return HGCONFIG
-
-
 def register_blueprints(app):
     from views.main import main as main_blueprint
+    from views.api import api as api_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(api_blueprint, url_prefix='/api')
