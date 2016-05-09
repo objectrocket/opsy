aa0eaa0a82634b483e1410ca097d36c117129f7f
Chris Jowett <chris.jowett@rackspace.com>
2016-05-09 14:57:36 -0500

    Initial work for poller greenlet
    Signed-off-by: Chris Jowett <chris.jowett@rackspace.com>

--- hourglass/__init__.py
+++ hourglass/__init__.py
@@ -1,5 +1,7 @@
+import gevent
 from flask import Flask
 from flask.ext.iniconfig import INIConfig
+from models.poller import Poller
 
 
 def create_app(config):
@@ -8,9 +10,12 @@ def create_app(config):
     app.config.from_inifile(config)
     parse_config(app)
     register_blueprints(app)
-    print app.config
+    create_poller(app)
     return app
 
+def create_poller(app):
+    poller = Poller(app)
+    return gevent.spawn(poller.run)
 
 def parse_config(app):
     hourglass_config = app.config.get('hourglass')
