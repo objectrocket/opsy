adf50f6467a8efc3d6ac6e86d14baa3d80ccf44c
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-10 17:33:25 -0500

    Testing custom query class (#36)
    * started custom query class
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>
    
    * MOMMY, WHY IS THERE SQL IN MAH API?
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>
    
    * list comp ftw
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/__init__.py
+++ hourglass/models/__init__.py
@@ -1,9 +1,20 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy, BaseQuery
+from flask import json
 
 
 db = SQLAlchemy()
 
 
-class HourglassMixin(object):
+class JsonOut(BaseQuery):
+
+    def all_extra_as_dict(self):
+        return [json.loads(x.extra) for x in self]
+
+    def all_extra_as_json(self):
+        return json.dumps(self.all_extra_as_dict())
+
+
+class HourglassCacheMixin(object):
+    query_class = JsonOut
     updated_at = db.Column(db.DateTime, default=db.func.now(),
                            onupdate=db.func.now())
--- hourglass/views/api/views.py
+++ hourglass/views/api/views.py
@@ -1,7 +1,7 @@
 from time import time
 from . import api
-from hourglass.models.api import get_events, get_checks
-from flask import current_app, json
+from hourglass.models.api import *
+from flask import json
 
 
 @api.route('/ping')
@@ -12,12 +12,18 @@ def ping():
 @api.route('/events')
 @api.route('/events/<datacenter>')
 def events(datacenter=None):
-    sensuevents = get_events(current_app.config, datacenter)
+    filter_list = []
+    if datacenter:
+        filter_list.append(Event.datacenter.in_([datacenter]))
+    sensuevents = Event.query.filter(*filter_list).all_extra_as_dict()
     return json.dumps({'events': sensuevents, 'timestamp': time()})
 
 
 @api.route('/checks')
 @api.route('/checks/<datacenter>')
 def checks(datacenter=None):
-    sensuchecks = get_checks(current_app.config, datacenter)
+    filter_list = []
+    if datacenter:
+        filter_list.append(Check.datacenter.in_([datacenter]))
+    sensuchecks = Check.query.filter(*filter_list).all_extra_as_dict()
     return json.dumps({'checks': sensuchecks, 'timestamp': time()})
