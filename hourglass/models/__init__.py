3b4f75d10442d42c613abed3070f73821bc2e372
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-10 15:52:53 -0500

    started custom query class
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/__init__.py
+++ hourglass/models/__init__.py
@@ -1,9 +1,15 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy, BaseQuery
 
 
 db = SQLAlchemy()
 
 
+class JsonOut(BaseQuery):
+    def as_json(self):
+        return "this is a test"
+
+
 class HourglassMixin(object):
     updated_at = db.Column(db.DateTime, default=db.func.now(),
                            onupdate=db.func.now())
+    query_class = JsonOut
