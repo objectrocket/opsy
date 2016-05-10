04e5bbdabc61442da223597a7b89ff70ca04e27d
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-10 17:31:52 -0500

    list comp ftw
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/__init__.py
+++ hourglass/models/__init__.py
@@ -8,10 +8,7 @@ db = SQLAlchemy()
 class JsonOut(BaseQuery):
 
     def all_extra_as_dict(self):
-        out_list = []
-        for x in self:
-            out_list.append(json.loads(x.extra))
-        return out_list
+        return [json.loads(x.extra) for x in self]
 
     def all_extra_as_json(self):
         return json.dumps(self.all_extra_as_dict())
