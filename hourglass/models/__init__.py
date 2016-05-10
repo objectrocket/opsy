86c99914f454166e3cf02a9b4bdb15c79558adc2
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-10 17:17:21 -0500

    MOMMY, WHY IS THERE SQL IN MAH API?
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/__init__.py
+++ hourglass/models/__init__.py
@@ -1,15 +1,23 @@
 from flask.ext.sqlalchemy import SQLAlchemy, BaseQuery
+from flask import json
 
 
 db = SQLAlchemy()
 
 
 class JsonOut(BaseQuery):
-    def as_json(self):
-        return "this is a test"
+
+    def all_extra_as_dict(self):
+        out_list = []
+        for x in self:
+            out_list.append(json.loads(x.extra))
+        return out_list
+
+    def all_extra_as_json(self):
+        return json.dumps(self.all_extra_as_dict())
 
 
-class HourglassMixin(object):
+class HourglassCacheMixin(object):
+    query_class = JsonOut
     updated_at = db.Column(db.DateTime, default=db.func.now(),
                            onupdate=db.func.now())
-    query_class = JsonOut
