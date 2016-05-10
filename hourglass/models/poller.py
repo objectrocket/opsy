7a6d19f44f4ce9e508f6df14d44e524dccb694d9
cryptk <cryptk@gmail.com>
2016-05-10 12:41:32 -0500

    Add in poller/api caching of Events and Checks (#27)
    Signed-off-by: Chris Jowett <chris.jowett@rackspace.com>

--- hourglass/models/poller.py
+++ hourglass/models/poller.py
@@ -2,7 +2,7 @@ import gevent
 import gevent.monkey
 import requests
 from flask import json
-from hourglass.models.api import Client
+from hourglass.models.api import Check, Client, Event
 from . import db
 
 gevent.monkey.patch_all()
@@ -22,22 +22,52 @@ class Poller(object):
     def get_clients(cls, sensu):
         return json.loads(requests.get('http://'+sensu['host']+':'+str(sensu['port'])+'/clients').content)
 
+    @classmethod
+    def get_checks(cls, sensu):
+        return json.loads(requests.get('http://'+sensu['host']+':'+str(sensu['port'])+'/checks').content)
+
+    @classmethod
+    def get_events(cls, sensu):
+        return json.loads(requests.get('http://'+sensu['host']+':'+str(sensu['port'])+'/events').content)
+
+    def update_checks_cache(self, checks):
+        with self.app.app_context():
+            Check.query.delete()
+            for sensu in checks:
+                for check in checks[sensu]:
+                    db.session.add(Check(sensu, check['name'], check))
+            db.session.commit()
+
     def update_clients_cache(self, clients):
         with self.app.app_context():
             Client.query.delete()
             for sensu in clients:
                 for client in clients[sensu]:
                     try:
-                        db.session.add(Client(sensu, client['name'], client, client['timestamp']))
+                        db.session.add(Client(sensu, client['name'], client['timestamp'], client))
                     except KeyError:
-                        db.session.add(Client(sensu, client['name'], client, 0))
+                        db.session.add(Client(sensu, client['name'], 0, client))
+            db.session.commit()
+
+    def update_events_cache(self, events):
+        with self.app.app_context():
+            Event.query.delete()
+            for sensu in events:
+                for event in events[sensu]:
+                    db.session.add(Event(sensu, event['client']['name'], event['check']['name'], event['occurrences'], event['check']['status'], event['timestamp'], event))
             db.session.commit()
 
     def main(self):
+        checks = {}
         clients = {}
+        events = {}
         for sensu in self.sensus:
+            checks[sensu] = self.get_checks(self.sensus[sensu])
             clients[sensu] = self.get_clients(self.sensus[sensu])
+            events[sensu] = self.get_events(self.sensus[sensu])
         self.update_clients_cache(clients)
+        self.update_checks_cache(checks)
+        self.update_events_cache(events)
 
     def run(self):
         while True:
