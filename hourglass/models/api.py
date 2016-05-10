4e24d6bb4ba5c736284936f1764998efb712793b
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-10 17:33:25 -0500

    Testing custom query class (#36)
    * started custom query class
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>
    
    * MOMMY, WHY IS THERE SQL IN MAH API?
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>
    
    * list comp ftw
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/api.py
+++ hourglass/models/api.py
@@ -1,52 +1,9 @@
 from datetime import datetime
-from time import time
-import requests
-from . import db, HourglassMixin
+from flask import json
+from . import db, HourglassCacheMixin
 
 
-def get_events(config, datacenter=None):
-    # Temp functions for front-end testing until we get the caching and poller
-    # running
-    sensu_nodes = config.get('sensu_nodes')
-    hourglass_config = config.get('hourglass')
-    events = []
-    if datacenter:
-        hosts = {datacenter: sensu_nodes.get(datacenter)}
-    else:
-        hosts = sensu_nodes
-    for name, details in hosts.iteritems():
-        url = 'http://%s:%s/events' % (details['host'], details['port'])
-        localevents = requests.get(url).json()
-        for event in localevents:
-            age = int(time() - event['timestamp'])
-            event.update({'datacenter': name})
-        events += localevents
-    for event in events:
-        uchiwa_url = '%s/#/client/%s/%s?check=%s' % (
-            hourglass_config['uchiwa_url'], event['datacenter'],
-            event['client']['name'], event['check']['name'])
-        event.update({
-            'lastcheck': age,
-            'href': uchiwa_url})
-    return events
-
-
-def get_checks(config, datacenter=None):
-    # Temp functions for front-end testing until we get the caching and poller
-    # running
-    sensu_nodes = config.get('sensu_nodes')
-    checks = []
-    if datacenter:
-        hosts = {datacenter: sensu_nodes.get(datacenter)}
-    else:
-        hosts = sensu_nodes
-    for name, details in hosts.iteritems():
-        url = 'http://%s:%s/checks' % (details['host'], details['port'])
-        localchecks = requests.get(url).json()
-        for check in localchecks:
-            check.update({'datacenter': name})
-        checks += localchecks
-    return checks
+# UCHIWA_URL = db.get_app().config.get('uchiwa_url')
 
 
 # Laying out the framework for the api caching layer.
@@ -55,13 +12,13 @@ def get_checks(config, datacenter=None):
 #     __tablename__ = 'nodes'
 
 
-class Client(HourglassMixin, db.Model):
+class Client(HourglassCacheMixin, db.Model):
     __bind_key__ = 'cache'
     __tablename__ = 'clients'
     datacenter = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(256), primary_key=True)
     timestamp = db.Column(db.DateTime)
-    extra = db.Column(db.PickleType)
+    extra = db.Column(db.Text)
 
     events = db.relationship('Event', backref='client', lazy='dynamic')
 
@@ -69,7 +26,9 @@ class Client(HourglassMixin, db.Model):
         self.datacenter = datacenter
         self.name = name
         extra['datacenter'] = datacenter
-        self.extra = extra
+        # extra['href'] = '%s/#/client/%s/%s' % (UCHIWA_URL, self.datacenter,
+        #                                        self.name)
+        self.extra = json.dumps(extra)
         try:
             self.timestamp = datetime.fromtimestamp(timestamp)
         except TypeError:
@@ -79,18 +38,18 @@ class Client(HourglassMixin, db.Model):
         return '<Client %s/%s>' % (self.datacenter, self.name)
 
 
-class Check(HourglassMixin, db.Model):
+class Check(HourglassCacheMixin, db.Model):
     __bind_key__ = 'cache'
     __tablename__ = 'checks'
     datacenter = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(256), primary_key=True)
-    extra = db.Column(db.PickleType)
+    extra = db.Column(db.Text)
 
     def __init__(self, datacenter, name, extra):
         self.datacenter = datacenter
         self.name = name
         extra['datacenter'] = datacenter
-        self.extra = extra
+        self.extra = json.dumps(extra)
 
     def __repr__(self):
         return '<Check %s/%s>' % (self.datacenter, self.name)
@@ -106,7 +65,7 @@ class Check(HourglassMixin, db.Model):
 #     __tablename__ = 'aggregates'
 
 
-class Event(HourglassMixin, db.Model):
+class Event(HourglassCacheMixin, db.Model):
     __bind_key__ = 'cache'
     __tablename__ = 'events'
     datacenter = db.Column(db.String(64), primary_key=True)
@@ -117,7 +76,7 @@ class Event(HourglassMixin, db.Model):
     eventoccurrences = db.Column(db.BigInteger)
     status = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime)
-    extra = db.Column(db.PickleType)
+    extra = db.Column(db.Text)
 
     __table_args__ = (
         db.ForeignKeyConstraint(
@@ -126,8 +85,8 @@ class Event(HourglassMixin, db.Model):
         ),
     )
 
-
-    def __init__(self, datacenter, clientname, occurrences, status, timestamp, extra):
+    def __init__(self, datacenter, clientname, occurrences, status, timestamp,
+                 extra):
         self.datacenter = datacenter
         self.clientname = clientname
         self.checkname = extra['check']['name']
@@ -137,10 +96,13 @@ class Event(HourglassMixin, db.Model):
         self.status = status
         self.timestamp = datetime.fromtimestamp(timestamp)
         extra['datacenter'] = datacenter
-        self.extra = extra
+        # extra['href'] = '%s/#/client/%s/%s?check=%s' % (
+        #     UCHIWA_URL, self.datacenter, self.clientname, self.checkname)
+        self.extra = json.dumps(extra)
 
     def __repr__(self):
-        return '<Event %s/%s/%s>' % (self.datacenter, self.clientname, self.checkname)
+        return '<Event %s/%s/%s>' % (self.datacenter, self.clientname,
+                                     self.checkname)
 
 
 # class Stash(db.Model):
