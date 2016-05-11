63ebcd61ab029ef02e9541110b63b96d4c2fc2d3
cryptk <cryptk@gmail.com>
2016-05-11 14:14:55 -0500

    Change refresh interval back to 30 seconds

--- hourglass/static/js/hourglass/events.js
+++ hourglass/static/js/hourglass/events.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
                 updateDataTablesUrl()
                 updateFilters();
                 document.eventstable.ajax.reload(null, false);
-            }, 3000);
+            }, 30000);
         }
     });
 });
