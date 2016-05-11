1afa78e7b586540cad798ddd24a90770b01fd155
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-11 11:32:22 -0500

    Working on api (#38)
    * switching to jsonify since it sets the proper mime type
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>
    
    * done with filtering at the api level
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/views/api/views.py
+++ hourglass/views/api/views.py
@@ -1,29 +1,58 @@
 from time import time
 from . import api
-from hourglass.models.api import Event, Check
-from flask import json
+from hourglass.models.api import Event, Check, Client
+from flask import jsonify, request
+
+
+def get_filters_list(filters):
+    filters_list = []
+    for items, db_object in filters:
+        if items:
+            item_list = items.split(',')
+            include = [x for x in item_list if not x.startswith('!')]
+            exclude = [x[1:] for x in item_list if x.startswith('!')]
+            if include:
+                filters_list.append(db_object.in_(include))
+            if exclude:
+                filters_list.append(~db_object.in_(exclude))
+    return filters_list
 
 
 @api.route('/ping')
 def ping():
-    return json.dumps({'pong': time()})
+    return jsonify({'pong': time()})
 
 
 @api.route('/events')
-@api.route('/events/<datacenter>')
-def events(datacenter=None):
-    filter_list = []
-    if datacenter:
-        filter_list.append(Event.datacenter.in_([datacenter]))
-    sensuevents = Event.query.filter(*filter_list).all_extra_as_dict()
-    return json.dumps({'events': sensuevents, 'timestamp': time()})
+def events():
+    datacenters = request.args.get("datacenter")
+    checknames = request.args.get("checkname")
+    clientnames = request.args.get("clientname")
+    filters = ((datacenters, Event.datacenter),
+               (checknames, Event.checkname),
+               (clientnames, Event.clientname))
+    filters_list = get_filters_list(filters)
+    sensuevents = Event.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'events': sensuevents, 'timestamp': time()})
 
 
 @api.route('/checks')
-@api.route('/checks/<datacenter>')
-def checks(datacenter=None):
-    filter_list = []
-    if datacenter:
-        filter_list.append(Check.datacenter.in_([datacenter]))
-    sensuchecks = Check.query.filter(*filter_list).all_extra_as_dict()
-    return json.dumps({'checks': sensuchecks, 'timestamp': time()})
+def checks():
+    datacenters = request.args.get("datacenter")
+    checknames = request.args.get("checkname")
+    filters = ((datacenters, Check.datacenter),
+               (checknames, Check.name))
+    filters_list = get_filters_list(filters)
+    sensuchecks = Check.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'checks': sensuchecks, 'timestamp': time()})
+
+
+@api.route('/clients')
+def clients():
+    datacenters = request.args.get("datacenter")
+    clientnames = request.args.get("clientname")
+    filters = ((datacenters, Client.datacenter),
+               (clientnames, Client.name))
+    filters_list = get_filters_list(filters)
+    sensuclients = Client.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'clients': sensuclients, 'timestamp': time()})
