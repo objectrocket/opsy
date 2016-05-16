a385d9476292a9530e5b9a98643d2413486bd7cc
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-16 16:07:31 -0500

    Reorganize structure (#70)
    Moving the main templates and static files under the main view. Updated
    the paths in the templates to use url_for so they're more dynamic.
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/views/main/__init__.py
+++ hourglass/views/main/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__)  # pylint: disable=invalid-name
+main = Blueprint('main', __name__, template_folder='templates',
+                 static_url_path='/static/main',
+                 static_folder='static')  # pylint: disable=invalid-name
 
 from . import views  # pylint: disable=wrong-import-position
