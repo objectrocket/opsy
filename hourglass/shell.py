2b12dd6a3cd62fdca552d781516f9d5b09cea292
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-17 16:06:12 -0500

    fixing an import
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/shell.py
+++ hourglass/shell.py
@@ -1,6 +1,6 @@
 import os
 from flask.ext.script import Manager, Shell
-from models.backends.sensu.cache import Client, Check, Event, Stash, db
+from hourglass.models.backends.sensu.cache import Client, Check, Event, Stash, db
 from . import create_app
 import gevent.monkey
 gevent.monkey.patch_all()
