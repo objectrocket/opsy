3da6e5e40c79ee8967d8a8d3caa07e32d9c2f8d8
Chris Jowett <chris.jowett@rackspace.com>
2016-05-17 13:27:59 -0500

    Remove unneeded path information from views
    Signed-off-by: Chris Jowett <chris.jowett@rackspace.com>

--- hourglass/views/main/__init__.py
+++ hourglass/views/main/__init__.py
@@ -1,10 +1,9 @@
 from flask import Blueprint
 from os.path import abspath, join, dirname
 
-datapath = abspath(dirname(__file__))
 
-main = Blueprint('main', __name__, template_folder=join(datapath, 'templates'),
+main = Blueprint('main', __name__, template_folder='templates',
                  static_url_path='/static/main',
-                 static_folder=join(datapath, 'static'))  # pylint: disable=invalid-name
+                 static_folder='static')  # pylint: disable=invalid-name
 
 from . import views  # pylint: disable=wrong-import-position
