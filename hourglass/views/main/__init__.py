b5be084cf95c2025f4ecf6eaff0587f61f0bfc07
Chris Jowett <cryptk@gmail.com>
2016-05-16 19:24:17 -0500

    Initial addition of setuptools functionality
      - Should address #74
      - Could probably use a second pass to make sure everything is right
    
    Signed-off-by: Chris Jowett <cryptk@gmail.com>

--- hourglass/views/main/__init__.py
+++ hourglass/views/main/__init__.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from os.path import abspath, join, dirname
 
-main = Blueprint('main', __name__, template_folder='templates',
+datapath = abspath(dirname(__file__))
+
+main = Blueprint('main', __name__, template_folder=join(datapath, 'templates'),
                  static_url_path='/static/main',
-                 static_folder='static')  # pylint: disable=invalid-name
+                 static_folder=join(datapath, 'static'))  # pylint: disable=invalid-name
 
 from . import views  # pylint: disable=wrong-import-position
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hourglass',
+    version='0.1',
+    packages=find_packages(),
+    description='A multi-user/group web dashboard for Sensu',
+    author='Chris Jowett, David Bennett',
+    url='https://github.com/cryptk/hourglass',
+    license='MIT',
+    install_requires = [
+        'Flask==0.10.1',
+        'requests==2.10.0',
+        'Flask-Script==2.0.5',
+        'humanize==0.5.1',
+        'Flask-INIConfig==0.0.9',
+        'Flask-SQLAlchemy==2.1',
+        'gevent==1.1.1',
+    ],
+    extras_require = {
+        'DEV': [
+            'ipython==4.2.0',
+        ],
+        'test': [
+            'pylint==1.5.5',
+            'pylint-flask==0.3',
+            'pep8==1.7.0',
+        ],
+    },
+    entry_points = {
+        'console_scripts': [
+            'hourglass = hourglass.command_line:run_hourglass',
+        ]
+    },
+    include_package_data = True,
+)
