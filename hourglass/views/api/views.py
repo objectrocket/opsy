df7f18b838dea1a4bdd32b06244f25d3904b3b03
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-18 17:05:10 -0500

    Made the cache models more simplified
    Now models only need two things to initialize them. This will likely need
    to change when we add support for other backends, but that's future David's
    problem.
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/views/api/views.py
+++ hourglass/views/api/views.py
@@ -1,7 +1,7 @@
 from time import time
 from . import api
 from hourglass.models.backends import db
-from hourglass.models.backends.sensu.cache import Event, Check, Client, Stash
+from hourglass.models.backends.sensu.cache import *
 from flask import current_app, jsonify, request
 
 
@@ -30,12 +30,12 @@ def parse_include_excludes(items):
 
 def get_dashboard_filters_list(config, dashboard):
     datacenters = config['dashboards'][dashboard].get('datacenter')
-    checknames = config['dashboards'][dashboard].get('checkname')
-    clientnames = config['dashboards'][dashboard].get('clientname')
+    check_names = config['dashboards'][dashboard].get('check_name')
+    client_names = config['dashboards'][dashboard].get('client_name')
     statuses = config['dashboards'][dashboard].get('status')
     filters = ((datacenters, Event.datacenter),
-               (checknames, Event.checkname),
-               (clientnames, Event.clientname),
+               (check_names, Event.check_name),
+               (client_names, Event.client_name),
                (statuses, Event.status))
     return get_filters_list(filters)
 
@@ -69,9 +69,9 @@ def list_checks():
         events_query = Event.query.filter(*dash_filters_list)
     else:
         events_query = Event.query
-    eventchecks = [x[0] for x in events_query.with_entities(
-        Event.checkname).distinct().all()]
-    return jsonify({'checks': eventchecks, 'timestamp': time()})
+    event_checks = [x[0] for x in events_query.with_entities(
+        Event.check_name).distinct().all()]
+    return jsonify({'checks': event_checks, 'timestamp': time()})
 
 
 @api.route('/events')
@@ -79,68 +79,83 @@ def events():
     dashboard = request.args.get('dashboard')
     hide_silenced = request.args.get('hide_silenced') or ''
     datacenters = request.args.get('datacenter')
-    checknames = request.args.get('checkname')
-    clientnames = request.args.get('clientname')
+    check_names = request.args.get('check_name')
+    client_names = request.args.get('client_name')
     statuses = request.args.get('status')
     filters = ((datacenters, Event.datacenter),
-               (checknames, Event.checkname),
-               (clientnames, Event.clientname),
+               (check_names, Event.check_name),
+               (client_names, Event.client_name),
                (statuses, Event.status))
     filters_list = get_filters_list(filters)
     hide_silenced = hide_silenced.split(',')
     if 'checks' in hide_silenced:
         filters_list.append(db.not_(Event.stash.has(
-            clientname=Event.clientname, checkname=Event.checkname,
+            client_name=Event.client_name, check_name=Event.check_name,
             flavor='silence')))
     if 'clients' in hide_silenced:
         filters_list.append(db.not_(Client.stash.has(
-            clientname=Event.clientname, checkname=None,
+            client_name=Event.client_name, check_name=None,
             flavor='silence')))
     if 'occurrences' in hide_silenced:
         filters_list.append(db.not_(
-            Event.eventoccurrences < Event.checkoccurrences))
+            Event.event_occurrences < Event.check_occurrences))
     if dashboard:
         config = current_app.config
         dash_filters_list = get_dashboard_filters_list(config, dashboard)
         events_query = Event.query.filter(*dash_filters_list)
     else:
         events_query = Event.query
-    sensuevents = events_query.filter(*filters_list).all_extra_as_dict()
-    return jsonify({'events': sensuevents, 'timestamp': time()})
+    sensu_events = events_query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'events': sensu_events, 'timestamp': time()})
 
 
 @api.route('/checks')
 def checks():
     datacenters = request.args.get('datacenter')
-    checknames = request.args.get('checkname')
+    check_names = request.args.get('check_name')
     filters = ((datacenters, Check.datacenter),
-               (checknames, Check.name))
+               (check_names, Check.name))
     filters_list = get_filters_list(filters)
-    sensuchecks = Check.query.filter(*filters_list).all_extra_as_dict()
-    return jsonify({'checks': sensuchecks, 'timestamp': time()})
+    sensu_checks = Check.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'checks': sensu_checks, 'timestamp': time()})
 
 
 @api.route('/clients')
 def clients():
     datacenters = request.args.get('datacenter')
-    clientnames = request.args.get('clientname')
+    client_names = request.args.get('client_name')
     filters = ((datacenters, Client.datacenter),
-               (clientnames, Client.name))
+               (client_names, Client.name))
     filters_list = get_filters_list(filters)
-    sensuclients = Client.query.filter(*filters_list).all_extra_as_dict()
-    return jsonify({'clients': sensuclients, 'timestamp': time()})
+    sensu_clients = Client.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'clients': sensu_clients, 'timestamp': time()})
+
+
+@api.route('/results')
+def results():
+    datacenters = request.args.get('datacenter')
+    check_names = request.args.get('check_name')
+    client_names = request.args.get('client_name')
+    statuses = request.args.get('status')
+    filters = ((datacenters, Result.datacenter),
+               (check_names, Result.check_name),
+               (client_names, Result.client_name),
+               (statuses, Result.status))
+    filters_list = get_filters_list(filters)
+    sensu_results = Result.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'results': sensu_results, 'timestamp': time()})
 
 
 @api.route('/stashes')
 def stashes():
     datacenters = request.args.get('datacenter')
-    clientnames = request.args.get('clientname')
-    checknames = request.args.get('checkname')
+    client_names = request.args.get('client_name')
+    check_names = request.args.get('check_name')
     flavor = request.args.get('flavor')
     filters = ((datacenters, Stash.datacenter),
-               (clientnames, Stash.clientname),
-               (checknames, Stash.checkname),
+               (client_names, Stash.client_name),
+               (check_names, Stash.check_name),
                (flavor, Stash.flavor))
     filters_list = get_filters_list(filters)
-    sensustashes = Stash.query.filter(*filters_list).all_extra_as_dict()
-    return jsonify({'stashes': sensustashes, 'timestamp': time()})
+    sensu_stashes = Stash.query.filter(*filters_list).all_extra_as_dict()
+    return jsonify({'stashes': sensu_stashes, 'timestamp': time()})
