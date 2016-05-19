31499d645edad6636981377063cedbd4659b8fde
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-19 12:45:29 -0500

    Made the cache models more simplified (#83)
    Now models only need two things to initialize them. This will likely need
    to change when we add support for other backends, but that's future David's
    problem.
    
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/backends/sensu/cache.py
+++ hourglass/models/backends/sensu/cache.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import json
 from .. import db, ExtraOut, HourglassCacheMixin
 
@@ -14,7 +13,6 @@ class Client(HourglassCacheMixin, db.Model):
     __tablename__ = 'clients'
     datacenter = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(256), primary_key=True)
-    timestamp = db.Column(db.DateTime)
     extra = db.Column(db.Text)
 
     events = db.relationship('Event', backref='client', lazy='dynamic',
@@ -23,19 +21,15 @@ class Client(HourglassCacheMixin, db.Model):
     __table_args__ = (
         db.ForeignKeyConstraint(
             ['datacenter', 'name'],
-            ['stashes.datacenter', 'stashes.clientname']
+            ['stashes.datacenter', 'stashes.client_name']
         ),
     )
 
-    def __init__(self, datacenter, name, extra, timestamp=None):
+    def __init__(self, datacenter, extra):
         self.datacenter = datacenter
-        self.name = name
+        self.name = extra['name']
         extra['datacenter'] = datacenter
         self.extra = json.dumps(extra)
-        try:
-            self.timestamp = datetime.fromtimestamp(timestamp)
-        except TypeError:
-            self.timestamp = None
 
     def __repr__(self):
         return '<Client %s/%s>' % (self.datacenter, self.name)
@@ -48,9 +42,9 @@ class Check(HourglassCacheMixin, db.Model):
     name = db.Column(db.String(256), primary_key=True)
     extra = db.Column(db.Text)
 
-    def __init__(self, datacenter, name, extra):
+    def __init__(self, datacenter, extra):
         self.datacenter = datacenter
-        self.name = name
+        self.name = extra['name']
         extra['datacenter'] = datacenter
         self.extra = json.dumps(extra)
 
@@ -58,9 +52,38 @@ class Check(HourglassCacheMixin, db.Model):
         return '<Check %s/%s>' % (self.datacenter, self.name)
 
 
-# class Result(db.Model):
-#     __bind_key__ = 'cache'
-#     __tablename__ = 'results'
+class Result(HourglassCacheMixin, db.Model):
+    __bind_key__ = 'cache'
+    __tablename__ = 'results'
+    datacenter = db.Column(db.String(64), primary_key=True)
+    client_name = db.Column(db.String(256), primary_key=True)
+    check_name = db.Column(db.String(256), primary_key=True)
+    status = db.Column(db.Integer)
+    extra = db.Column(db.Text)
+
+    __table_args__ = (
+        db.ForeignKeyConstraint(
+            ['datacenter', 'client_name'],
+            ['clients.datacenter', 'clients.name']
+        ),
+        db.ForeignKeyConstraint(
+            ['datacenter', 'client_name', 'check_name'],
+            ['stashes.datacenter', 'stashes.client_name', 'stashes.check_name']
+        )
+    )
+
+    def __init__(self, datacenter, extra):
+        self.datacenter = datacenter
+        self.extra = extra
+        self.client_name = extra['client']
+        self.check_name = extra['check']['name']
+        self.status = extra['check']['status']
+        extra['datacenter'] = datacenter
+        self.extra = json.dumps(extra)
+
+    def __repr__(self):
+        return '<Result %s/%s/%s>' % (self.datacenter, self.client_name,
+                                      self.check_name)
 
 
 # class Aggregate(db.Model):
@@ -72,42 +95,37 @@ class Event(HourglassCacheMixin, db.Model):
     __bind_key__ = 'cache'
     __tablename__ = 'events'
     datacenter = db.Column(db.String(64), primary_key=True)
-    clientname = db.Column(db.String(256), primary_key=True)
-    checkname = db.Column(db.String(256), primary_key=True)
-    checkoutput = db.Column(db.Text)
-    checkoccurrences = db.Column(db.BigInteger)
-    eventoccurrences = db.Column(db.BigInteger)
+    client_name = db.Column(db.String(256), primary_key=True)
+    check_name = db.Column(db.String(256), primary_key=True)
+    check_occurrences = db.Column(db.BigInteger)
+    event_occurrences = db.Column(db.BigInteger)
     status = db.Column(db.Integer)
-    timestamp = db.Column(db.DateTime)
     extra = db.Column(db.Text)
 
     __table_args__ = (
         db.ForeignKeyConstraint(
-            ['datacenter', 'clientname'],
+            ['datacenter', 'client_name'],
             ['clients.datacenter', 'clients.name']
         ),
         db.ForeignKeyConstraint(
-            ['datacenter', 'clientname', 'checkname'],
-            ['stashes.datacenter', 'stashes.clientname', 'stashes.checkname']
+            ['datacenter', 'client_name', 'check_name'],
+            ['stashes.datacenter', 'stashes.client_name', 'stashes.check_name']
         )
     )
 
-    def __init__(self, datacenter, clientname, occurrences, status, timestamp,
-                 extra):
+    def __init__(self, datacenter, extra):
         self.datacenter = datacenter
-        self.clientname = clientname
-        self.checkname = extra['check']['name']
-        self.checkoutput = extra['check']['output']
-        self.checkoccurrences = extra['check'].get('occurrences')
-        self.eventoccurrences = occurrences
-        self.status = status
-        self.timestamp = datetime.fromtimestamp(timestamp)
+        self.client_name = extra['client']['name']
+        self.check_name = extra['check']['name']
+        self.check_occurrences = extra['check'].get('occurrences')
+        self.event_occurrences = extra['occurrences']
+        self.status = extra['check']['status']
         extra['datacenter'] = datacenter
         self.extra = json.dumps(extra)
 
     def __repr__(self):
-        return '<Event %s/%s/%s>' % (self.datacenter, self.clientname,
-                                     self.checkname)
+        return '<Event %s/%s/%s>' % (self.datacenter, self.client_name,
+                                     self.check_name)
 
 
 class Stash(HourglassCacheMixin, db.Model):
@@ -115,12 +133,9 @@ class Stash(HourglassCacheMixin, db.Model):
     __tablename__ = 'stashes'
     datacenter = db.Column(db.String(64), primary_key=True)
     path = db.Column(db.String(256), primary_key=True)
-    clientname = db.Column(db.String(256), primary_key=True)
-    checkname = db.Column(db.String(256), nullable=True, primary_key=True)
-    source = db.Column(db.String(64))
+    client_name = db.Column(db.String(256), primary_key=True)
+    check_name = db.Column(db.String(256), nullable=True, primary_key=True)
     flavor = db.Column(db.String(64))
-    created_at = db.Column(db.DateTime)
-    expire_at = db.Column(db.DateTime)
     extra = db.Column(db.Text)
 
     events = db.relationship('Event', backref='stash', lazy='dynamic',
@@ -128,34 +143,30 @@ class Stash(HourglassCacheMixin, db.Model):
     client = db.relationship('Client', backref='stash', lazy='dynamic',
                              query_class=ExtraOut, viewonly=True)
 
-    def __init__(self, datacenter, path, extra):
+    def __init__(self, datacenter, extra):
         self.datacenter = datacenter
-        self.path = path
-        extra['datacenter'] = datacenter
-        self.extra = json.dumps(extra)
-        if extra.get('expire') == -1:
-            self.expire_at = None
-        else:
-            self.expire_at = datetime.fromtimestamp(
-                int(datetime.utcnow().strftime("%s")) + extra['expire'])
+        self.path = extra['path']
         try:
-            path_list = path.split('/')
+            path_list = self.path.split('/')
             self.flavor = path_list[0]
-            self.clientname = path_list[1]
+            self.client_name = path_list[1]
             try:
-                self.checkname = path_list[2]
+                self.check_name = path_list[2]
             except IndexError:
-                self.checkname = None
+                self.check_name = None
             self.source = extra['content']['source']
-            self.created_at = datetime.fromtimestamp(
-                extra['content']['timestamp'])
         except:
             self.flavor = None
-            self.clientname = None
-            self.checkname = None
+            self.client_name = None
+            self.check_name = None
             self.source = None
             self.created_at = None
             self.expire_at = None
+        extra['datacenter'] = self.datacenter
+        extra['check_name'] = self.check_name
+        extra['client_name'] = self.client_name
+        extra['flavor'] = self.flavor
+        self.extra = json.dumps(extra)
 
     def __repr__(self):
         return '<Stash %s/%s>' % (self.datacenter, self.path)
