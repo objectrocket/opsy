6f8843cef15b5fc114b0f9e807a7789ce243c3b7
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-24 11:04:48 -0500

    fixing the poller so it polls things (#88)
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/models/poller.py
+++ hourglass/models/poller.py
@@ -1,80 +1,55 @@
 import asyncio
 import aiohttp
-from hourglass.models.backends.sensu.cache import Check, Client, Event, Stash
+from hourglass.models.backends.sensu.cache import Check, Client, Event, Stash, Result
 from hourglass.models.backends import db
 
 
 class Poller(object):
     def __init__(self, app):
+        self.sensu_models = [Check, Client, Event, Stash, Result]
         self.app = app
         self.db = db
-        self.httpsession = aiohttp.ClientSession()
         self.sensus = self.app.config['sensu_nodes']
         self.interval = self.app.config['hourglass'].get('poll_interval', 10)
         self.loop = asyncio.get_event_loop()
+        self.httpsession = aiohttp.ClientSession(loop=self.loop)
 
     @asyncio.coroutine
-    def query_sensu(self, sensu, uri):
-        url = "http://%s:%s/%s" % (sensu['host'], sensu['port'], uri)
-        with aiohttp.Timeout(sensu['timeout']):
-            response = yield from self.httpsession.get(url)
-        return (yield from response.json())
+    def query_sensu(self, details, uri):
+        timeout = int(details.get('timeout', 5))
+        url = "http://%s:%s/%s" % (details['host'], details['port'], uri)
+        out_dict = {}
+        try:
+            with aiohttp.Timeout(timeout):
+                response = yield from self.httpsession.get(url)
+                out_dict = yield from response.json()
+                response.release()
+        except:
+            self.app.logger.debug("Reached timeout on request to %s" % url)
+        return (out_dict)
 
     @asyncio.coroutine
-    def update_sensu_checks(self, sensu):
-        self.app.logger.debug("updating checks " + sensu['name'])
-        checks = yield from self.query_sensu(sensu, 'checks')
-        checkobjects = []
+    def update_sensu_object(self, name, details, model):
+        self.app.logger.debug("Updating %s cache for %s" % (
+            model.__tablename__, name))
+        checks = yield from self.query_sensu(details, model.__tablename__)
+        init_objects = []
         for check in checks:
-            checkobjects.append(Check(sensu['name'], check))
-        self.app.logger.debug("updated checks " + sensu['name'])
-        return checkobjects
+            init_objects.append(model(name, check))
+        self.app.logger.debug("Updated %s cache for %s" % (
+            model.__tablename__, name))
+        return init_objects
 
-    @asyncio.coroutine
-    def update_sensu_clients(self, sensu):
-        self.app.logger.debug("updating clients " + sensu['name'])
-        clients = yield from self.query_sensu(sensu, 'clients')
-        clientobjects = []
-        for client in clients:
-            clientobjects.append(Client(sensu['name'], client))
-        self.app.logger.debug("updated clients " + sensu['name'])
-        return clientobjects
-
-    @asyncio.coroutine
-    def update_sensu_events(self, sensu):
-        self.app.logger.debug("updating events " + sensu['name'])
-        events = yield from self.query_sensu(sensu, 'events')
-        eventobjects = []
-        for event in events:
-            eventobjects.append(Event(sensu['name'], event))
-        self.app.logger.debug("updated events " + sensu['name'])
-        return eventobjects
-
-    @asyncio.coroutine
-    def update_sensu_stashes(self, sensu):
-        self.app.logger.debug("updating stashes " + sensu['name'])
-        stashes = yield from self.query_sensu(sensu, 'stashes')
-        stashobjects = []
-        for stash in stashes:
-            stashobjects.append(Stash(sensu['name'], stash))
-        self.app.logger.debug("updated stashes " + sensu['name'])
-        return stashobjects
-
-    def main(self):
+    def update_cache(self):
         self.app.logger.debug('Updating Cache')
-        tasks = []
-        for sensu in self.sensus:
-            tasks.append(asyncio.ensure_future(self.update_sensu_checks(self.sensus[sensu])))
-            tasks.append(asyncio.ensure_future(self.update_sensu_clients(self.sensus[sensu])))
-            tasks.append(asyncio.ensure_future(self.update_sensu_events(self.sensus[sensu])))
-            tasks.append(asyncio.ensure_future(self.update_sensu_stashes(self.sensus[sensu])))
         with self.app.app_context():
+            tasks = []
+            for name, details in self.sensus.items():
+                for model in self.sensu_models:
+                    self.app.logger.debug('Purging cache %s for %s' % (model.__tablename__, name))
+                    model.query.filter(model.datacenter == name).delete()
+                    tasks.append(asyncio.async(self.update_sensu_object(name, details, model)))
             self.loop.run_until_complete(asyncio.wait(tasks))
-            for sensu in self.sensus:
-                Check.query.filter(Check.datacenter==sensu).delete()
-                Client.query.filter(Client.datacenter==sensu).delete()
-                Event.query.filter(Event.datacenter==sensu).delete()
-                Stash.query.filter(Stash.datacenter==sensu).delete()
             for task in tasks:
                 self.db.session.bulk_save_objects(task.result())
             self.db.session.commit()
--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,13 @@ from hourglass import create_app
 from hourglass.models.poller import Poller
 from uwsgidecorators import timer
 
+
 app = create_app('./hourglass.ini')
 poller = Poller(app)
-poller.main()
+poller.update_cache()
 poll_interval = app.config['hourglass'].get('poll_interval', 10)
 
 
 @timer(poll_interval)
 def run_poller(signum):
-    poller.main()
+    poller.update_cache()
