7acb105a1558bddc3111cd67f4f97be767712a86
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-25 23:31:24 -0500

    Make aiohttp great again
    Reworked some logic in the scheduler and poller to make aiohttp actually work now. Still needs some more love to make it not eat your data when it has a boom boom https://www.youtube.com/watch?v=uHLKVZcurVc
    
    Also removed the non-uwsgi scheduler.
    
    Signed-off-by: M. David Bennett mdavidbennett@syntheticworks.com

--- hourglass/backends/poller.py
+++ hourglass/backends/poller.py
@@ -1,12 +1,14 @@
 import asyncio
+import aiohttp
 from hourglass.backends.cache import *
-from hourglass.backends import db
 
 
 class Poller(object):
 
-    def __init__(self, name, config):
-        self.models = [Check, Client, Event, Stash, Result]
+    def __init__(self, app, loop, name, config):
+        self.models = [Check, Client, Event, Stash]
+        self.app = app
+        self.loop = loop
         self.name = name
         self.host = config.get('host')
         self.port = config.get('port')
@@ -16,34 +18,27 @@ class Poller(object):
         raise NotImplementedError
 
     @asyncio.coroutine
-    def update_objects(self, app, model):
-        app.logger.debug('Updating %s cache for %s' % (
+    def update_objects(self, model):
+        self.app.logger.debug('Updating %s cache for %s' % (
             model.__tablename__, self.name))
-        results = self.query_api(app, model.uri)
-        if results is None:
-            # bail out and don't touch the existing cache if something broke
-            return []
-        app.logger.debug('Purging cache %s for %s' % (
+        with aiohttp.ClientSession(loop=self.loop) as session:
+            results = yield from self.query_api(session, model.uri)
+        self.app.logger.debug('Purging cache %s for %s' % (
             model.__tablename__, self.name))
         model.query.filter(model.datacenter == self.name).delete()
         init_objects = []
         for result in results:
             init_objects.append(model(self.name, result))
-        app.logger.debug('Updated %s cache for %s' % (
+        self.app.logger.debug('Updated %s cache for %s' % (
             model.__tablename__, self.name))
         return init_objects
 
-    @asyncio.coroutine
-    def update_cache(self, app):
-        app.logger.debug('Updating Cache for %s' % self.name)
+    def get_update_tasks(self):
+        self.app.logger.info('Updating Cache for %s' % self.name)
         tasks = []
         for model in self.models:
-            tasks.append(asyncio.async(self.update_objects(app, model)))
-        results = yield from asyncio.gather(*tasks)
-        for result in results:
-            db.session.bulk_save_objects(result)
-        db.session.commit()
-        app.logger.debug('Cache update complete for %s' % self.name)
+            tasks.append(asyncio.async(self.update_objects(model)))
+        return tasks
 
     def __repr__(self):
         return '<Poller base/%s>' % self.name
