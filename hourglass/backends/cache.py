8a825f63e439e4114f0bc780f2a60eb1f6224f2e
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-05-27 17:53:39 -0500

    bunch of fun stuff with the api (#95)
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/backends/cache.py
+++ hourglass/backends/cache.py
@@ -1,17 +1,97 @@
+import asyncio
 from flask import json
+from hourglass.utils import get_filters_list
 from . import db, ExtraOut, HourglassCacheMixin
 
 
-# Laying out the framework for the api caching layer.
-# class Node(db.Model):
-#     __bind_key__ = 'cache'
-#     __tablename__ = 'nodes'
+class Zone(db.Model):
+
+    __bind_key__ = 'cache'
+    __tablename__ = 'zones'
+
+    name = db.Column(db.String(64), primary_key=True)
+    backend_type = db.Column(db.String(64))
+    host = db.Column(db.String(64))
+    port = db.Column(db.Integer())
+    timeout = db.Column(db.Integer())
+    updated_at = db.Column(db.DateTime, default=db.func.now(),
+                           onupdate=db.func.now())
+
+    meta_data = db.relationship('ZoneMetadata', backref='zone', lazy='dynamic',
+                                query_class=ExtraOut)
+    clients = db.relationship('Client', backref='zone', lazy='dynamic',
+                              query_class=ExtraOut)
+    checks = db.relationship('Check', backref='zone', lazy='dynamic',
+                             query_class=ExtraOut)
+    events = db.relationship('Event', backref='zone', lazy='dynamic',
+                             query_class=ExtraOut)
+    results = db.relationship('Result', backref='zone', lazy='dynamic',
+                              query_class=ExtraOut)
+    stashes = db.relationship('Stash', backref='zone', lazy='dynamic',
+                              query_class=ExtraOut)
+
+    def __init__(self, name, host, port, timeout):
+        self.models = [Check, Client, Event, Stash, Result]
+        self.backend_type = None
+        self.name = name
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
+    def query_api(self, uri):
+        raise NotImplementedError
+
+    @asyncio.coroutine
+    def update_objects(self, model):
+        raise NotImplementedError
+
+    def get_update_tasks(self, app, loop):
+        tasks = []
+        for model in self.models:
+            tasks.append(asyncio.async(self.update_objects(app, loop, model)))
+        return tasks
+
+    @classmethod
+    def get_dashboard_filters_list(cls, config, dashboard):
+        if config['dashboards'].get(dashboard) is None:
+            return ()
+        zones = config['dashboards'][dashboard].get('zone')
+        filters = ((zones, cls.name),)
+        return get_filters_list(filters)
+
+    def __repr__(self):
+        return '<Zone %s>' % self.name
+
+
+class ZoneMetadata(db.Model):
+
+    __bind_key__ = 'cache'
+    __tablename__ = 'zone_metadata'
+
+    zone_name = db.Column(db.String(64), primary_key=True)
+    key = db.Column(db.String(64))
+    value = db.Column(db.String(64))
+    updated_at = db.Column(db.DateTime, default=db.func.now(),
+                           onupdate=db.func.now())
+
+    __table_args__ = (db.ForeignKeyConstraint(['zone_name'], ['zones.name']),)
+
+    def __init__(self, zone_name, key, value):
+        self.zone_name = zone_name
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return '<ZoneMetadata %s: %s - %s>' % (self.zone_name, self.key,
+                                               self.value)
 
 
 class Client(HourglassCacheMixin, db.Model):
+
     __bind_key__ = 'cache'
     __tablename__ = 'clients'
-    datacenter = db.Column(db.String(64), primary_key=True)
+
+    zone_name = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(256), primary_key=True)
     extra = db.Column(db.Text)
 
@@ -19,43 +99,70 @@ class Client(HourglassCacheMixin, db.Model):
                              query_class=ExtraOut)
 
     __table_args__ = (
+        db.ForeignKeyConstraint(['zone_name'], ['zones.name']),
         db.ForeignKeyConstraint(
-            ['datacenter', 'name'],
-            ['stashes.datacenter', 'stashes.client_name']
-        ),
+            ['zone_name', 'name'],
+            ['stashes.zone_name', 'stashes.client_name']
+        )
     )
 
-    def __init__(self, datacenter, extra):
-        self.datacenter = datacenter
+    def __init__(self, zone_name, extra):
+        self.zone_name = zone_name
         self.name = extra['name']
-        extra['datacenter'] = datacenter
+        extra['zone_name'] = zone_name
         self.extra = json.dumps(extra)
 
+    @classmethod
+    def get_dashboard_filters_list(cls, config, dashboard):
+        if config['dashboards'].get(dashboard) is None:
+            return ()
+        zones = config['dashboards'][dashboard].get('zone')
+        clients = config['dashboards'][dashboard].get('client')
+        filters = ((zones, cls.zone_name),
+                   (clients, cls.name))
+        return get_filters_list(filters)
+
     def __repr__(self):
-        return '<Client %s/%s>' % (self.datacenter, self.name)
+        return '<Client %s/%s>' % (self.zone_name, self.name)
 
 
 class Check(HourglassCacheMixin, db.Model):
+
     __bind_key__ = 'cache'
     __tablename__ = 'checks'
-    datacenter = db.Column(db.String(64), primary_key=True)
+
+    zone_name = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(256), primary_key=True)
     extra = db.Column(db.Text)
 
-    def __init__(self, datacenter, extra):
-        self.datacenter = datacenter
+    __table_args__ = (db.ForeignKeyConstraint(['zone_name'], ['zones.name']),)
+
+    def __init__(self, zone_name, extra):
+        self.zone_name = zone_name
         self.name = extra['name']
-        extra['datacenter'] = datacenter
+        extra['zone_name'] = zone_name
         self.extra = json.dumps(extra)
 
+    @classmethod
+    def get_dashboard_filters_list(cls, config, dashboard):
+        if config['dashboards'].get(dashboard) is None:
+            return ()
+        zones = config['dashboards'][dashboard].get('zone')
+        checks = config['dashboards'][dashboard].get('check')
+        filters = ((zones, cls.zone_name),
+                   (checks, cls.name))
+        return get_filters_list(filters)
+
     def __repr__(self):
-        return '<Check %s/%s>' % (self.datacenter, self.name)
+        return '<Check %s/%s>' % (self.zone_name, self.name)
 
 
 class Result(HourglassCacheMixin, db.Model):
+
     __bind_key__ = 'cache'
     __tablename__ = 'results'
-    datacenter = db.Column(db.String(64), primary_key=True)
+
+    zone_name = db.Column(db.String(64), primary_key=True)
     client_name = db.Column(db.String(256), primary_key=True)
     check_name = db.Column(db.String(256), primary_key=True)
     status = db.Column(db.Integer)
@@ -63,26 +170,44 @@ class Result(HourglassCacheMixin, db.Model):
 
     __table_args__ = (
         db.ForeignKeyConstraint(
-            ['datacenter', 'client_name'],
-            ['clients.datacenter', 'clients.name']
+            ['zone_name'],
+            ['zones.name']
         ),
         db.ForeignKeyConstraint(
-            ['datacenter', 'client_name', 'check_name'],
-            ['stashes.datacenter', 'stashes.client_name', 'stashes.check_name']
+            ['zone_name', 'client_name'],
+            ['clients.zone_name', 'clients.name']
+        ),
+        db.ForeignKeyConstraint(
+            ['zone_name', 'client_name', 'check_name'],
+            ['stashes.zone_name', 'stashes.client_name', 'stashes.check_name']
         )
     )
 
-    def __init__(self, datacenter, extra):
-        self.datacenter = datacenter
+    def __init__(self, zone_name, extra):
+        self.zone_name = zone_name
         self.extra = extra
         self.client_name = extra['client']
         self.check_name = extra['check']['name']
         self.status = extra['check']['status']
-        extra['datacenter'] = datacenter
+        extra['zone_name'] = zone_name
         self.extra = json.dumps(extra)
 
+    @classmethod
+    def get_dashboard_filters_list(cls, config, dashboard):
+        if config['dashboards'].get(dashboard) is None:
+            return ()
+        zones = config['dashboards'][dashboard].get('zone')
+        checks = config['dashboards'][dashboard].get('check')
+        clients = config['dashboards'][dashboard].get('client')
+        statuses = config['dashboards'][dashboard].get('status')
+        filters = ((zones, cls.zone_name),
+                   (checks, cls.check_name),
+                   (clients, cls.client_name),
+                   (statuses, cls.status))
+        return get_filters_list(filters)
+
     def __repr__(self):
-        return '<Result %s/%s/%s>' % (self.datacenter, self.client_name,
+        return '<Result %s/%s/%s>' % (self.zone_name, self.client_name,
                                       self.check_name)
 
 
@@ -92,9 +217,11 @@ class Result(HourglassCacheMixin, db.Model):
 
 
 class Event(HourglassCacheMixin, db.Model):
+
     __bind_key__ = 'cache'
     __tablename__ = 'events'
-    datacenter = db.Column(db.String(64), primary_key=True)
+
+    zone_name = db.Column(db.String(64), primary_key=True)
     client_name = db.Column(db.String(256), primary_key=True)
     check_name = db.Column(db.String(256), primary_key=True)
     check_occurrences = db.Column(db.BigInteger)
@@ -103,48 +230,67 @@ class Event(HourglassCacheMixin, db.Model):
     extra = db.Column(db.Text)
 
     __table_args__ = (
+        db.ForeignKeyConstraint(['zone_name'], ['zones.name']),
         db.ForeignKeyConstraint(
-            ['datacenter', 'client_name'],
-            ['clients.datacenter', 'clients.name']
+            ['zone_name', 'client_name'],
+            ['clients.zone_name', 'clients.name']
         ),
         db.ForeignKeyConstraint(
-            ['datacenter', 'client_name', 'check_name'],
-            ['stashes.datacenter', 'stashes.client_name', 'stashes.check_name']
+            ['zone_name', 'client_name', 'check_name'],
+            ['stashes.zone_name', 'stashes.client_name', 'stashes.check_name']
         )
     )
 
-    def __init__(self, datacenter, extra):
-        self.datacenter = datacenter
+    def __init__(self, zone_name, extra):
+        self.zone_name = zone_name
         self.client_name = extra['client']['name']
         self.check_name = extra['check']['name']
         self.check_occurrences = extra['check'].get('occurrences')
         self.event_occurrences = extra['occurrences']
         self.status = extra['check']['status']
-        extra['datacenter'] = datacenter
+        extra['zone_name'] = zone_name
         self.extra = json.dumps(extra)
 
+    @classmethod
+    def get_dashboard_filters_list(cls, config, dashboard):
+        if config['dashboards'].get(dashboard) is None:
+            return ()
+        zones = config['dashboards'][dashboard].get('zone')
+        checks = config['dashboards'][dashboard].get('check')
+        clients = config['dashboards'][dashboard].get('client')
+        statuses = config['dashboards'][dashboard].get('status')
+        filters = ((zones, cls.zone_name),
+                   (checks, cls.check_name),
+                   (clients, cls.client_name),
+                   (statuses, cls.status))
+        return get_filters_list(filters)
+
     def __repr__(self):
-        return '<Event %s/%s/%s>' % (self.datacenter, self.client_name,
+        return '<Event %s/%s/%s>' % (self.zone_name, self.client_name,
                                      self.check_name)
 
 
 class Stash(HourglassCacheMixin, db.Model):
+
     __bind_key__ = 'cache'
     __tablename__ = 'stashes'
-    datacenter = db.Column(db.String(64), primary_key=True)
+
+    zone_name = db.Column(db.String(64), primary_key=True)
     path = db.Column(db.String(256), primary_key=True)
     client_name = db.Column(db.String(256), primary_key=True)
     check_name = db.Column(db.String(256), nullable=True, primary_key=True)
     flavor = db.Column(db.String(64))
     extra = db.Column(db.Text)
 
+    __table_args__ = (db.ForeignKeyConstraint(['zone_name'], ['zones.name']),)
+
     events = db.relationship('Event', backref='stash', lazy='dynamic',
                              query_class=ExtraOut, viewonly=True)
     client = db.relationship('Client', backref='stash', lazy='dynamic',
                              query_class=ExtraOut, viewonly=True)
 
-    def __init__(self, datacenter, extra):
-        self.datacenter = datacenter
+    def __init__(self, zone_name, extra):
+        self.zone_name = zone_name
         self.path = extra['path']
         try:
             path_list = self.path.split('/')
@@ -162,11 +308,11 @@ class Stash(HourglassCacheMixin, db.Model):
             self.source = None
             self.created_at = None
             self.expire_at = None
-        extra['datacenter'] = self.datacenter
+        extra['zone_name'] = self.zone_name
         extra['check_name'] = self.check_name
         extra['client_name'] = self.client_name
         extra['flavor'] = self.flavor
         self.extra = json.dumps(extra)
 
     def __repr__(self):
-        return '<Stash %s/%s>' % (self.datacenter, self.path)
+        return '<Stash %s/%s>' % (self.zone_name, self.path)
