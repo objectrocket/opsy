42e9ee302bbdb46ff65fa1e8aef014bbecb0512f
cryptk <cryptk@gmail.com>
2016-05-31 15:37:26 -0500

    Re-implement Checks page (#96)
    Signed-off-by: Chris Jowett <cryptk@gmail.com>

--- hourglass/views/main/static/js/hourglass.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('table.dt').DataTable({
-        'lengthMenu': [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ],
-        'stateSave' : true,
-    });
-    $('.link-row').click(function() {
-        window.document.location = $(this).data("href");
-    });
-});
