95f9563d50ade4e5d2f2459d0eb2ab5a0e212761
Chris Jowett <cryptk@gmail.com>
2016-06-13 15:44:20 -0500

    Implement clients details page (#113)
    Signed-off-by: Chris Jowett <cryptk@gmail.com>

---
+++ hourglass/views/main/static/js/hourglass/client_details.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    $('#clientdetails').html(function() {
+        $.getJSON('/api/clients/'+ZONE+'/'+CLIENT, function(data) {
+            client = data['clients'][0];
+            $('#clientdetails').html('<h4>'+client['name']+'</h4><pre>'+JSON.stringify(client,null,2)+'</pre>');
+        })
+    })
+    document.clientevents = $('#clientevents').DataTable({
+        'lengthMenu': [ [25, 50, 100, -1], [25, 50, 100, "All"] ],
+        'order': [
+            [ 0, 'asc' ],
+            [ 1, 'desc' ],
+        ],
+        'ajax': {
+            'url': '/api/clients/'+ZONE+'/'+CLIENT+'/results',
+            'dataSrc': function(json) {
+                json = json['results'];
+                return_data = new Array();
+                for (var i=0; i<json.length; i++) {
+                    var row = json[i];
+                    row['check']['status'] = hourglass.statusnames[row['check']['status']];
+                    if ( row['check']['status'] === undefined ) {
+                        row['check']['status'] = 'Unknown';
+                    }
+                    var d = new Date(0);
+                    d.setUTCSeconds(row['check']['issued']);
+                    return_data.push({
+                        'status': row['check']['status'],
+                        'check_name': row['check']['name'],
+                        'check_output': row['check']['output'],
+                        'timestamp': '<time class="timeago" datetime="'+d.toISOString()+'">'+d+'</time>',
+                    })
+                }
+                console.log(json);
+                return return_data;
+            }
+        },
+        'columns': [
+            {data: 'status'},
+            {data: 'check_name'},
+            {data: 'check_output'},
+            {data: 'timestamp'},
+        ],
+        'columnDefs': [
+            { type: 'enum', targets: [0]},
+        ],
+        'rowCallback': function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
+            $('td:first', nRow).addClass(hourglass.statusclasses[aData['status']]);
+        },
+    }).on('draw.dt', function() {
+        $('time.timeago').timeago();
+    });
+})
