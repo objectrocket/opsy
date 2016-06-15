24d843b74c855dbc6bfb6231e6530cda629449b6
M. David Bennett <mdavidbennett@syntheticworks.com>
2016-06-15 18:02:38 -0500

    At this point I don't know what I'm doing (#119)
    Signed-off-by: M. David Bennett <mdavidbennett@syntheticworks.com>

--- hourglass/__init__.py
+++ hourglass/__init__.py
@@ -17,12 +17,6 @@ def create_app(config):
     parse_config(app)
     db.init_app(app)
     app.scheduler = UwsgiScheduler(app)
-    try:
-        import uwsgi
-        if uwsgi.worker_id() == 1:
-            app.scheduler.create_cache_db()
-    except:
-        pass
     register_blueprints(app)
     return app
 
--- hourglass/backends/sensu/cache.py
+++ hourglass/backends/sensu/cache.py
@@ -5,50 +5,6 @@ from ..cache import *
 from . import SensuBase
 
 
-class SensuZone(SensuBase, Zone):
-
-    def __init__(self, name, host, port, timeout):
-        self.models = [SensuCheck, SensuClient, SensuEvent, SensuStash,
-                       SensuResult]
-        self.name = name
-        self.host = host
-        self.port = port
-        self.timeout = timeout
-
-    @asyncio.coroutine
-    def query_api(self, session, url):
-        with aiohttp.Timeout(self.timeout):
-            response = yield from session.get(url)
-            return (yield from response.json())
-
-    @asyncio.coroutine
-    def update_objects(self, app, loop, model):
-        init_objects = []
-        try:
-            with aiohttp.ClientSession(loop=loop) as session:
-                url = "http://%s:%s/%s" % (self.host, self.port, model.uri)
-                app.logger.debug('Making request to %s' % url)
-                results = yield from self.query_api(session, url)
-        except Exception as e:
-            app.logger.error('Error updating %s cache for %s: %s' % (
-                model.__tablename__, self.name, e))
-            return init_objects
-        model.query.filter(model.zone_name == self.name).delete()
-        for result in results:
-            init_objects.append(model(self.name, result))
-        app.logger.info('Updated %s cache for %s' % (
-            model.__tablename__, self.name))
-        return init_objects
-
-
-class SensuZoneMetadata(SensuBase, ZoneMetadata):
-
-    def __init__(self, zone_name, key, value):
-        self.zone_name = zone_name
-        self.key = key
-        self.value = value
-
-
 class SensuClient(SensuBase, Client):
     uri = 'clients'
 
@@ -123,3 +79,48 @@ class SensuStash(SensuBase, Stash):
         extra['client_name'] = self.client_name
         extra['flavor'] = self.flavor
         self.extra = json.dumps(extra)
+
+
+class SensuZoneMetadata(SensuBase, ZoneMetadata):
+
+    def __init__(self, zone_name, key, value):
+        self.zone_name = zone_name
+        self.key = key
+        self.value = value
+
+
+class SensuZone(SensuBase, Zone):
+
+    models = [SensuCheck, SensuClient, SensuEvent, SensuStash, SensuResult]
+
+    def __init__(self, name, host, port, timeout):
+        self.name = name
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
+    @asyncio.coroutine
+    def query_api(self, session, url):
+        with aiohttp.Timeout(self.timeout):
+            response = yield from session.get(url)
+            return (yield from response.json())
+
+    @asyncio.coroutine
+    def update_objects(self, app, loop, model):
+        init_objects = []
+        try:
+            with aiohttp.ClientSession(loop=loop) as session:
+                url = "http://%s:%s/%s" % (self.host, self.port, model.uri)
+                app.logger.debug('Making request to %s' % url)
+                results = yield from self.query_api(session, url)
+        except Exception as e:
+            app.logger.error('Error updating %s cache for %s: %s' % (
+                model.__tablename__, self.name, e))
+            app.logger.error(dir(e))
+            return init_objects
+        model.query.filter(model.zone_name == self.name).delete()
+        for result in results:
+            init_objects.append(model(self.name, result))
+        app.logger.info('Updated %s cache for %s' % (
+            model.__tablename__, self.name))
+        return init_objects
