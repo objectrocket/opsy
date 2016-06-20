dd3a64d09199c418dfe1b4569b8eb5e88e3189ae
Chris Jowett <cryptk@gmail.com>
2016-06-20 14:39:23 -0500

    Update event details page to work with 'OK' results (#123)
    Signed-off-by: Chris Jowett <chris.jowett@rackspace.com>

--- hourglass/views/main/static/js/hourglass/client_event_details.js
+++ hourglass/views/main/static/js/hourglass/client_event_details.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
     $('#clientdetails').html(function() {
-        $.getJSON('/api/clients/'+ZONE+'/'+CLIENT+'/events/'+CHECK, function(data) {
+        var jqxhr = $.getJSON('/api/clients/'+ZONE+'/'+CLIENT+'/events/'+CHECK, function(data) {
             event = data['events'][0];
             $('#clientdetails').html('<h4>'+event['client']['name']+'</h4><pre>'+JSON.stringify(event['client'],null,2)+'</pre>');
             $('#eventdetails').html('<h4>'+event['check']['name']+'</h4><pre>'+JSON.stringify(event['check'],null,2)+'</pre>');
+        })
+        .fail(function() {
+            $.getJSON('/api/clients/'+ZONE+'/'+CLIENT, function(data) {
+                client = data['clients'][0];
+                $('#clientdetails').html('<h4>'+client['name']+'</h4><pre>'+JSON.stringify(client,null,2)+'</pre>');
+            })
+            $.getJSON('/api/clients/'+ZONE+'/'+CLIENT+'/results/'+CHECK, function(data) {
+                event = data['results'][0];
+                $('#eventdetails').html('<h4>'+event['check']['name']+'</h4><pre>'+JSON.stringify(event['check'],null,2)+'</pre>');
+            })
         })
     })
 })
