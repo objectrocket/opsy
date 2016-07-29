11a80c8de9799160a1b523acdfa059ca1583d576
Chris Jowett <cryptk@gmail.com>
2016-07-28 19:02:24 -0500

    Create notification tray, fixes #145 (#146)
    Signed-off-by: Chris Jowett <cryptk@gmail.com>

--- hourglass/views/main/static/js/hourglass/hourglass.js
+++ hourglass/views/main/static/js/hourglass/hourglass.js
@@ -36,21 +36,72 @@ var hourglass = {
             for (var i=0; i<zones.length; i++) {
                 zone = zones[i];
                 if (zone.status != 'ok') {
-                    if ($('#alert-container').children('#alert-'+zone.name).length == 0) {
-                        $('#alert-container').append('<div id="alert-'+zone.name+'" class="alert alert-danger" ><strong>Failure!</strong> datacenter '+zone.name+' is not responding!</div>')
-                    }
+                    hourglass.notification.add(zone.name+' Poller Failure', '<strong>Failure!</strong> datacenter '+zone.name+' is not responding!', 'danger', true, zone.name+'-offline');
                 } else {
-                    if ($('#alert-container').children('#alert-'+zone.name).length > 0) {
-                        $('#alert-'+zone.name).remove();
-                    } 
+                    hourglass.notification.remove(zone.name+'-offline');
                 }
             }
         })
     },
 
-    registerTask: function(slug, interval, func) {
+    registerTask: function(slug, interval, func, run_now=false) {
         console.log('registering task: '+slug+' to run every '+interval+' ticks.');
         hourglass.tasks.push({'slug': slug, 'interval': interval, 'callback': func});
+        if (run_now) {
+            func();
+        }
+    },
+
+    notification: {
+
+        add: function(title, content, level='danger', dismissable=true, slug=null) {
+            if (slug == null) {
+                slug = title.toLowerCase().replace(/ /g,'-');
+            }
+            if ($('#notification-container').children('#notification-'+slug).length == 0) {
+                console.log('adding notification: '+slug);
+                $('#notification-container').append("<div id='notification-"+slug+"' class='notification-item alert alert-"+level+"'><h4 class='item-title'>"+title+"</h4><p class='item-info'>"+content+"</p></div>");
+                if (dismissable) {
+                    $('#notification-'+slug).click(function(e) {
+                        e.stopPropagation();
+                        hourglass.notification.remove(slug)
+                    });
+                }
+                hourglass.notification.update();
+                hourglass.notification.jingle();
+            }
+            return;
+        },
+
+        remove: function(slug) {
+            if ($('#notification-container').children('#notification-'+slug).length > 0) {
+                console.log('removing notification: '+slug);
+                $('#notification-container').children('#notification-'+slug).remove();
+                hourglass.notification.update();
+            }
+        },
+
+        update: function() {
+            container = $('#notification-container')
+            $('#notification-count').html($(container).children().length);
+            alertClasses = ['alert-success', 'alert-info', 'alert-warning', 'alert-danger'];
+            var notificationColor = '';
+            $(alertClasses).each(function(idx, obj) {
+                if ($(container).children('.'+obj).length > 0) {
+                    $('#notification-icon').addClass(obj);
+                } else {
+                    $('#notification-icon').removeClass(obj);
+                }
+            });
+        },
+
+        jingle: function() {
+            $('#notification-icon').addClass('animate')
+            setTimeout(function() {
+                $('#notification-icon').removeClass('animate')
+            }, 1100);
+        }
+
     }
 
 }
@@ -66,7 +117,7 @@ String.prototype.capitalize = function(all) {
 }
 
 $(document).ready(function() {
-    hourglass.registerTask('zone-check', 5, hourglass.check_zones);
+    hourglass.registerTask('zone-check', 5, hourglass.check_zones, true);
     (function($) {
         $.QueryString = (function(a) {
             if (a == "") return {};
