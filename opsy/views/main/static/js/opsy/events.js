26879ae1192ec15e83d4d093cfa9c728dfac6095
cryptk <cryptk@gmail.com>
2016-08-02 18:48:27 -0500

    Add in javascript style checking (#158)
    Signed-off-by: Chris Jowett <cryptk@gmail.com>

--- opsy/views/main/static/js/opsy/events.js
+++ opsy/views/main/static/js/opsy/events.js
@@ -1,177 +1,191 @@
 var eventsFilters = {
 
-    statusOptions: [
-        {label: 'Critical', title: 'Critical', value: 2},
-        {label: 'Warning', title: 'Warning', value: 1},
-        {label: 'OK', title: 'OK', value: 0},
-    ],
+  statusOptions: [
+    {label: 'Critical', title: 'Critical', value: 2},
+    {label: 'Warning', title: 'Warning', value: 1},
+    {label: 'OK', title: 'OK', value: 0},
+  ],
 
-    hideOptions: [
-        {label: 'Silenced Checks', title: 'Silenced Checks', value: 'checks'},
-        {label: 'Silenced Clients', title: 'Silenced Clients', value: 'clients'},
-        {label: 'Below Occurrence Threshold', title: 'Below Occurrence Threshold', value: 'occurrences'},
-    ],
+  hideOptions: [
+    {label: 'Silenced Checks', title: 'Silenced Checks', value: 'checks'},
+    {label: 'Silenced Clients', title: 'Silenced Clients', value: 'clients'},
+    {label: 'Below Occurrence Threshold', title: 'Below Occurrence Threshold',
+    value: 'occurrences'},
+  ],
 
-    zoneOptions: [],
+  zoneOptions: [],
 
-    checkOptions: [],
+  checkOptions: [],
 
-    multiselectOptions: {
-        buttonWidth: '100%',
-        enableFiltering: true,
-        enableCaseInsensitiveFiltering: true,
-        numberDisplayed: 1,
-        includeSelectAllOption: true,
-        buttonText: function(options, select) {
-            if (options.length == 1) {
-                return $(options[0]).attr('label');
-            } else if (options.length == $(select).children(options).length) {
-                return 'All items selected';
-            } else if (options.length > 1) {
-                return options.length+' items selected';
-            } else {
-                return $(select).data('name').replace('-', ' ').capitalize(true);
-            }
-        },
-        buttonTitle: function(options, select) {
-            return $(select).data('name').replace('-', ' ').capitalize(true);
-        },
-        onDropdownHidden: function() {
-            eventsFilters.setDataTablesUrl();
-            document.eventstable.ajax.reload(null, false);
-        },
+  multiselectOptions: {
+    buttonWidth: '100%',
+    enableFiltering: true,
+    enableCaseInsensitiveFiltering: true,
+    numberDisplayed: 1,
+    includeSelectAllOption: true,
+    buttonText: function(options, select) {
+      if (options.length == 1) {
+        return $(options[0]).attr('label');
+      } else if (options.length == $(select).children(options).length) {
+        return 'All items selected';
+      } else if (options.length > 1) {
+        return options.length + ' items selected';
+      } else {
+        return $(select).data('name').replace('-', ' ').capitalize(true);
+      }
     },
-
-    create: function() {
-        opsy.addFormGroup('status');
-        opsy.addFormGroup('zone');
-        opsy.addFormGroup('check');
-        opsy.addFormGroup('hide-events', 'hide_silenced');
-        $('#zone-filter').multiselect(this.multiselectOptions);
-        $('#check-filter').multiselect(this.multiselectOptions);
-        $('#status-filter').multiselect(this.multiselectOptions);
-        $('#status-filter').multiselect('dataprovider', this.statusOptions);
-        $('#hide-events-filter').multiselect(this.multiselectOptions);
-        $('#hide-events-filter').multiselect('dataprovider', this.hideOptions);
-        this.updateFilters();
+    buttonTitle: function(options, select) {
+      return $(select).data('name').replace('-', ' ').capitalize(true);
     },
-
-    updateFilters: function() {
-        this.updateZones();
-        this.updateChecks();
+    onDropdownHidden: function() {
+      eventsFilters.setDataTablesUrl();
+      document.eventstable.ajax.reload(null, false);
     },
+  },
 
-    updateZones: function() {
-        var self = this;
-        url = opsy.get_dashboard_url('/api/zones');
-        $.getJSON(url, function updateZonesJSONCB(data) {
-            newzones = []
-            $.each(data.zones, function updateZonesEachCB(idx, obj) {
-                newzones.push({label: obj.name, title: obj.name, value: obj.name});
-            });
-            self.zoneOptions = newzones;
-        }).success(function updateZonesSuccessCB() {
-            $('#zone-filter').multiselect('dataprovider', self.zoneOptions);
-            $('#zone-filter').multiselect('rebuild');
-        });
-    },
+  create: function() {
+    opsy.addFormGroup('status');
+    opsy.addFormGroup('zone');
+    opsy.addFormGroup('check');
+    opsy.addFormGroup('hide-events', 'hide_silenced');
+    $('#zone-filter').multiselect(this.multiselectOptions);
+    $('#check-filter').multiselect(this.multiselectOptions);
+    $('#status-filter').multiselect(this.multiselectOptions);
+    $('#status-filter').multiselect('dataprovider', this.statusOptions);
+    $('#hide-events-filter').multiselect(this.multiselectOptions);
+    $('#hide-events-filter').multiselect('dataprovider', this.hideOptions);
+    this.updateFilters();
+  },
 
-    updateChecks: function() {
-        var self = this;
-        url = opsy.get_dashboard_url('/api/events?count_checks');
-        $.getJSON(url, function updateChecksJSONCB(data) {
-            newchecks = []
-            $.each(data.events.sort(), function updateChecksEachCB(idx, obj) {
-                newchecks.push({label: obj.name+' ('+obj.count+')', title: obj.name, value: obj.name});
-            });
-            self.checkOptions = newchecks;
-        }).success(function updateChecksSuccessCB() {
-            $('#check-filter').multiselect('dataprovider', self.checkOptions);
-            $('#check-filter').multiselect('rebuild');
-        });
-    },
+  updateFilters: function() {
+    this.updateZones();
+    this.updateChecks();
+  },
 
-    setDataTablesUrl: function() {
-        params = {}
-        $('select.ms').each(function setDTUrlEachCB(idx, obj) {
-            params[$(obj).data('filter')] = $(obj).children('option:selected').map(function setDTUrlMapCB() { return this.value }).get().join(',')
-        });
-        if ( $.QueryString['dashboard'] ) {
-            params['dashboard'] = $.QueryString['dashboard'];
-        }
-        try {
-            document.eventstable.ajax.url('/api/events?'+$.param(params));
-        } catch(err) {
-        }
-        return '/api/events?'+$.param(params);
+  updateZones: function() {
+    var self = this;
+    url = opsy.getDashboardUrl('/api/zones');
+    $.getJSON(url, function(data) {
+      newzones = [];
+      $.each(data.zones, function(idx, obj) {
+        newzones.push({label: obj.name, title: obj.name, value: obj.name});
+      });
+      self.zoneOptions = newzones;
+    }).success(function() {
+      $('#zone-filter').multiselect('dataprovider', self.zoneOptions);
+      $('#zone-filter').multiselect('rebuild');
+    });
+  },
+
+  updateChecks: function() {
+    var self = this;
+    url = opsy.getDashboardUrl('/api/events?count_checks');
+    $.getJSON(url, function(data) {
+      newchecks = [];
+      $.each(data.events.sort(), function(idx, obj) {
+        newchecks.push({label: obj.name + ' (' + obj.count + ')',
+          title: obj.name, value: obj.name});
+      });
+      self.checkOptions = newchecks;
+    }).success(function() {
+      $('#check-filter').multiselect('dataprovider', self.checkOptions);
+      $('#check-filter').multiselect('rebuild');
+    });
+  },
+
+  setDataTablesUrl: function() {
+    params = {};
+    $('select.ms').each(function(idx, obj) {
+      params[$(obj).data('filter')] = $(obj).children('option:selected').map(
+        function() { return this.value; }
+      ).get().join(',');
+    });
+    if ($.QueryString.dashboard) {
+      params.dashboard = $.QueryString.dashboard;
     }
-}
+    try {
+      document.eventstable.ajax.url('/api/events?' + $.param(params));
+    } catch (err) {
+    }
+    return '/api/events?' + $.param(params);
+  }
+};
 
 var getStatusCount = function(state) {
-    return document.eventstable.column(0).data().filter( function getStatusCountFilterCB(value, idx){
-          return value == state ? true : false;
-    }).length
-}
+  return document.eventstable.column(0).data().filter(function(value, idx) {
+    return value == state ? true : false;
+  }).length;
+};
 
 var updateTitle = function() {
-    crits = getStatusCount("Critical");
-    warns = getStatusCount("Warning");
-    document.title = crits+' Critical, '+warns+' Warning | Events | Opsy';
-}
+  crits = getStatusCount('Critical');
+  warns = getStatusCount('Warning');
+  document.title = crits + ' Critical, ' + warns + ' Warning | Events | Opsy';
+};
 
-$(document).ready(function documentReadyCB() {
-    $.fn.dataTable.enum( [ 'Critical', 'Warning', 'OK' ] );
-    document.eventstable = $('#events').DataTable({
-        'lengthMenu': [ [25, 50, 100, -1], [25, 50, 100, "All"] ],
-        'autoWidth': false,
-        //'stateSave' : true,
-        'order': [
-            [ 0, 'asc' ],
-            [ 5, 'desc' ],
-        ],
-        'ajax': {
-            url: eventsFilters.setDataTablesUrl(),
-            dataSrc: function(json) {
-                json = json['events'];
-                return_data = new Array();
-                for (var i=0; i<json.length; i++) {
-                    var row = json[i];
-                    uchiwaHref = UCHIWA_URL+'/#/client/'+row['zone_name']+'/'+row['client_name']+'?check='+row['check_name'],
-                    return_data.push({
-                        'status': row['status'].capitalize(),
-                        'zone': row['zone_name'],
-                        'source': "<a href='"+uchiwaHref+"'><img src='/static/main/img/backends/sensu.ico'></img></a> <a href='/clients/"+row['zone_name']+"/"+row['client_name']+"'>"+row['client_name']+"</a>",
-                        'check_name': row['check_name'],
-                        'check_output': row['output'],
-                        'count': row['occurrences'],
-                        'timestamp': '<time class="timeago" datetime="'+row['updated_at']+'Z">'+row['updated_at']+'Z</time>',
-                    })
-                }
-                return return_data;
-            },
-        },
-        'dom': "<'row'<'col-sm-2'l><'col-sm-2'<'#status-filter-div'>><'col-sm-2'<'#zone-filter-div'>><'col-sm-2'<'#check-filter-div'>><'col-sm-2'<'#hide-events-filter-div'>><'col-sm-2'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>",
-        'columns': [
-            {data: 'status'},
-            {data: 'zone'},
-            {data: 'source'},
-            {data: 'check_name'},
-            {data: 'check_output'},
-            {data: 'count'},
-            {data: 'timestamp'},
-        ],
-        'rowCallback': function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-            $('td:first', nRow).addClass(opsy.statusclasses[aData['status']]);
-        },
-        'initComplete': function (foo) {
-            eventsFilters.create();
-            opsy.registerTask('update-events', 6, function() {
-                eventsFilters.updateFilters();
-                document.eventstable.ajax.reload(null, false);
-            });
+$(document).ready(function() {
+  $.fn.dataTable.enum(['Critical', 'Warning', 'OK']);
+  document.eventstable = $('#events').DataTable({
+    'lengthMenu': [[25, 50, 100, -1], [25, 50, 100, 'All']],
+    'autoWidth': false,
+    //'stateSave' : true,
+    'order': [
+      [0, 'asc'],
+      [5, 'desc'],
+    ],
+    'ajax': {
+      url: eventsFilters.setDataTablesUrl(),
+      dataSrc: function(json) {
+        json = json.events;
+        returnData = new Array();
+        for (var i = 0; i < json.length; i++) {
+          var row = json[i];
+          //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+          uchiwaHref = UCHIWA_URL + '/#/client/' + row.zone_name + '/' +
+            row.client_name + '?check=' + row.check_name,
+          returnData.push({
+            'status': row.status.capitalize(),
+            'zone': row.zone_name,
+            'source': '<a href="' + uchiwaHref + '"><img src="' +
+              '/static/main/img/backends/sensu.ico"></img></a>' +
+              '<a href="/clients/' + row.zone_name + '/' + row.client_name +
+              '">' + row.client_name + '</a>',
+            'check_name': row.check_name,
+            'check_output': row.output,
+            'count': row.occurrences,
+            'timestamp': '<time class="timeago" datetime="' + row.updated_at +
+              'Z">' + row.updated_at + 'Z</time>',
+            //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+          });
         }
-    }).on('draw.dt', function onDrawDTCB() {
-        updateTitle();
-        $('time.timeago').timeago();
-    });
+        return returnData;
+      },
+    },
+    'dom': '<"row"<"col-sm-2"l><"col-sm-2"<"#status-filter-div">><"col-sm-2"' +
+      '<"#zone-filter-div">><"col-sm-2"<"#check-filter-div">><"col-sm-2"' +
+      '<"#hide-events-filter-div">><"col-sm-2"f>><"row"<"col-sm-12"tr>><"row"' +
+      '<"col-sm-5"i><"col-sm-7"p>>',
+    'columns': [
+      {data: 'status'},
+      {data: 'zone'},
+      {data: 'source'},
+      {data: 'check_name'},
+      {data: 'check_output'},
+      {data: 'count'},
+      {data: 'timestamp'},
+    ],
+    'rowCallback': function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+      $('td:first', nRow).addClass(opsy.statusclasses[aData.status]);
+    },
+    'initComplete': function(foo) {
+      eventsFilters.create();
+      opsy.registerTask('update-events', 6, function() {
+        eventsFilters.updateFilters();
+        document.eventstable.ajax.reload(null, false);
+      });
+    }
+  }).on('draw.dt', function() {
+    updateTitle();
+    $('time.timeago').timeago();
+  });
 });
