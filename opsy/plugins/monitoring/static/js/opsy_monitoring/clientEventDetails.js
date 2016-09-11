39f4cfcf44d93e173dacb7c819977561fea98875
Chris Jowett <cryptk@gmail.com>
2016-09-11 13:44:18 -0500

    Cleanup details page in monitoring plugin
      - Autolinks URL's that are found in the output field, other
        fields can be added easily if needed.
      - Fixes #184
    
    Signed-off-by: Chris Jowett <cryptk@gmail.com>

--- opsy/plugins/monitoring/static/js/opsy_monitoring/clientEventDetails.js
+++ opsy/plugins/monitoring/static/js/opsy_monitoring/clientEventDetails.js
@@ -1,29 +1,42 @@
 var clientEventDetails = {
 
-  datatables: {
+  init: function() {
+    clientEventDetails.update.all();
+    opsy.task.register('update-event-data', 6, function() {
+      clientEventDetails.update.eventData();
+    });
+  },
 
-    init: function() {
-      $('#clientdetails').html(function() {
-        $.getJSON('/api/monitoring/clients/' + ZONE + '/' + CLIENT, function(data) {
-          client = data.clients[0];
-          $('#clientdetails').html('<h4>' + client.name + '</h4><pre>' +
-            JSON.stringify(client, null, 2) + '</pre>');
-        });
-        $.getJSON('/api/monitoring/clients/' + ZONE + '/' + CLIENT + '/events/' + CHECK,
+  update: {
+
+    all: function() {
+      clientEventDetails.update.clientData();
+      clientEventDetails.update.eventData();
+    },
+
+    clientData: function() {
+      $.getJSON(Flask.url_for('monitoring_api.clients') + '/' + ZONE + '/' + CLIENT, function(data) {
+        client = data.clients[0];
+        $('#clientdetailsname').html(client.name);
+        $('#clientdetailstable').html(opsyMonitoring.formatJSONToTable(client));
+      });
+    },
+
+    eventData: function() {
+      $.getJSON(Flask.url_for('monitoring_api.clients') + '/' + ZONE + '/' + CLIENT + '/events/' + CHECK,
+      function(data) {
+        event = data.events[0];
+        $('#eventdetailsname').html(event.check_name); //jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+        $('#eventdetailstable').html(opsyMonitoring.formatJSONToTable(event));
+      })
+      .fail(function() {
+        $.getJSON(Flask.url_for('monitoring_api.clients') + '/' + ZONE + '/' + CLIENT + '/results/' + CHECK,
         function(data) {
-          event = data.events[0];
-          $('#eventdetails').html('<h4>' + event.check_name + '</h4><pre>' + //jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
-            JSON.stringify(event, null, 2) + '</pre>');
-        })
-        .fail(function() {
-          $.getJSON('/api/monitoring/clients/' + ZONE + '/' + CLIENT + '/results/' + CHECK,
-          function(data) {
-            event = data.results[0];
-            $('#eventdetails').html('<h4>' + event.check_name + '</h4><pre>' + //jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
-              JSON.stringify(event, null, 2) + '</pre>');
-          });
+          event = data.results[0];
+          $('#eventdetailsname').html(event.check_name); //jscs:ignore requireCamelCaseOrUpperCaseIdentifiers
+          $('#eventdetailstable').html(opsyMonitoring.formatJSONToTable(event));
         });
       });
-    }
-  }
+    },
+  },
 };
